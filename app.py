9d35407ac13515051cd790879834a94f1bafeb0b
elraphty <elraphtyofficial@gmail.com>
2019-11-25 15:00:02 +0100

    after adding articles

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 
+Articles = Articles()
+
+# Index
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# About 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# Get Articles
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+#Single Articles
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
